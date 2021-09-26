06817cd52c8aad54793fd3b52e53277ffb9cc692
zy0111 <91326665+zy0111@users.noreply.github.com>
2021-09-26 21:03:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError, LineBotApiError
+    InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
+    MessageEvent, TextMessage, TextSendMessage,
 )
 
 app = Flask(__name__)
-
 line_bot_api = LineBotApi('Channel access token')
 handler = WebhookHandler('Channel secret')
 
@@ -43,33 +42,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-      # get user id when reply
-    user_id = event.source.user_id
-    print("user_id =", user_id)
-    
-    reply_msg = event.message.text+'\nyour User ID is '+user_id+\
-                    ' \n輸入「你好」會啟動reply_message回復「不錯喔」'
-    
-    if event.message.text=='你好':
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='不錯喔'))
-    
-    image_url = 'https://i.imgur.com/d3vfgZP.png'
-    try:
-        line_bot_api.push_message(user_id, ImageSendMessage(original_content_url=image_url, preview_image_url=image_url))
-    except LineBotApiError as e:
-        # error handle
-        raise e
-    else:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=reply_msg))
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
